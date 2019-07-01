a2a5dc82cdf22e1518433054e0c30a59364701f6
Donelle Reed <donellereed@gmail.com>
2019-07-01 22:37:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(beatle, instrument){
+  var playArray = []
+  for (var i = 0; i <= beatle.length - 1; i++){
+  playArray.push(beatle[i] + " plays " + instrument[i])
+  }
+  return playArray
+}
+
+function johnLennonFacts (factArr) {
+  var fanGirl = []
+  for (var i = 0; i <= factArr.length - 1; i++){
+  fanGirl.push(factArr[i] + '!!!')
+}
+ return fanGirl
+}
